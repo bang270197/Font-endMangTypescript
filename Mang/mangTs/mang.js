00765eed70c9902bd2d8c0676edace29371d2ea8
Nguyen Ngoc Bang <bang270197@gmail.com>
2019-08-26 09:15:26 +0700

    mangtypescript

---
+++ Mang/mangTs/mang.js
@@ -0,0 +1,18 @@
+var list = [1, 2, 3];
+var categories = ['sport', 'bang', 'car'];
+console.log('list');
+// list.forEach((num) =>
+//     console.log(num.toFixed(2))
+// );
+// categories.forEach((str) =>
+// console.log(str.indexOf('a'))
+// );
+// const listSquare = list.map(num => num * num);
+// console.log(listSquare);
+//loc chuoi co do lai lon hon 2
+// const result = categories.filter(str => str.length > 3)
+// console.log(result);
+var x;
+// Initialize it
+x = ["hello", 10]; // OK
+console.log(x[0].substr(2));
---
+++ Mang/mangTs/mang.ts
@@ -0,0 +1,23 @@
+const  list: number[]=[1,2,3];
+const categories: Array<string>=['sport','bang','car'];
+console.log('list');
+// list.forEach((num) =>
+//     console.log(num.toFixed(2))
+// );
+// categories.forEach((str) =>
+// console.log(str.indexOf('a'))
+// );
+
+// const listSquare = list.map(num => num * num);
+// console.log(listSquare);
+
+//loc chuoi co do lai lon hon 2
+// const result = categories.filter(str => str.length > 3)
+// console.log(result);
+
+let x: [string, number];
+// Initialize it
+x = ["hello", 10]; // OK
+console.log(x[0].substr(2));
+
+
